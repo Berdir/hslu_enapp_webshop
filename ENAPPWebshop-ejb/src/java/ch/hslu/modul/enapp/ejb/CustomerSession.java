bc1e1c5d8fbe2250d89c93e4b29cf3095179276c
Sascha Grossenbacher <saschagros@gmail.com>
2010-10-14 22:44:43 +0200

    Added SHA1 hashing of the password

--- ENAPPWebshop-ejb/src/java/ch/hslu/modul/enapp/ejb/CustomerSession.java
+++ ENAPPWebshop-ejb/src/java/ch/hslu/modul/enapp/ejb/CustomerSession.java
@@ -6,10 +6,17 @@
 package ch.hslu.modul.enapp.ejb;
 
 import ch.hslu.modul.enapp.entity.Customer;
+import ch.hslu.modul.enapp.lib.SHACalculator;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -22,13 +29,32 @@ public class CustomerSession {
     private EntityManager em;
 
     public void register(Customer customer) {
-        
+        try {
+            customer.setPassword(SHACalculator.SHA1(customer.getPassword()));
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(CustomerSession.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(CustomerSession.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        em.persist(customer);
     }
-    
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
 
+    public Customer login(String username, String password) {
 
-    
-    
+        Query query = em.createNamedQuery("Customer.findByUsername");
+        query.setParameter("username", username);
+        for (Customer customer : (List<Customer>)query.getResultList()) {
+            try {
+                if (customer.getPassword().equals(SHACalculator.SHA1(password))) {
+                    return customer;
+                }
+            } catch (NoSuchAlgorithmException ex) {
+                Logger.getLogger(CustomerSession.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (UnsupportedEncodingException ex) {
+                Logger.getLogger(CustomerSession.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return null;
+    }
 }
