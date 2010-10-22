2deeb7e02c087ba687454b97078adcd9f0cddaab
Sascha Grossenbacher <saschagros@gmail.com>
2010-10-22 13:30:57 +0200

    VIA SOAP!!!!!

--- ENAPPWebshop-war/src/java/ch/hslu/modul/enapp/webshop/ProductList.java
+++ ENAPPWebshop-war/src/java/ch/hslu/modul/enapp/webshop/ProductList.java
@@ -7,10 +7,12 @@ package ch.hslu.modul.enapp.webshop;
 
 import ch.hslu.modul.enapp.ejb.Products;
 import ch.hslu.modul.enapp.entity.Product;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
+import schemas.dynamics.microsoft.page.item.Item;
 
 /**
  *
@@ -25,5 +27,9 @@ public class ProductList {
     /** Creates a new instance of ProductList */
     public List<Product> getProducts() {
         return productsEJB.getProducts();
+    }
+
+    public List<Item> getList() {
+        return productsEJB.getItems();
     }
 }
